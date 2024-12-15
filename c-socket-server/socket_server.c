aed5f00ab5f901a48ffbfad098df4d506e140aea
senthil4321L <senthil.jogi@gmail.com>
2024-12-15 10:40:31 +0100

    implement basic TCP socket server with signal handling and client communication

--- c-socket-server/socket_server.c
+++ c-socket-server/socket_server.c
@@ -1,6 +1,69 @@
 #include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <string.h>
+#include <signal.h>
+
+#define PORT 9876
+#define BUFFER_SIZE 1024
+
+int server_fd;
+
+void handle_signal(int signal) {
+    if (signal == SIGINT) {
+        printf("\nShutting down server...\n");
+        close(server_fd);
+        exit(0);
+    }
+}
 
 int main() {
-    printf("Hello, World!\n");
+    int new_socket;
+    struct sockaddr_in address;
+    int addrlen = sizeof(address);
+    char buffer[BUFFER_SIZE] = {0};
+    char *hello = "Hello from server";
+
+    // Creating socket file descriptor
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+        perror("socket failed");
+        return -1;
+    }
+
+    // Forcefully attaching socket to the port 9876
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
+
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+        perror("bind failed");
+        return -1;
+    }
+
+    if (listen(server_fd, 3) < 0) {
+        perror("listen");
+        return -1;
+    }
+
+    signal(SIGINT, handle_signal);
+
+    while (1) {
+        printf("Waiting for a connection...\n");
+        if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) {
+            perror("accept");
+            return -1;
+        }
+
+        read(new_socket, buffer, BUFFER_SIZE);
+        printf("Message from client: %s\n", buffer);
+        send(new_socket, hello, strlen(hello), 0);
+        printf("Hello message sent\n");
+
+        close(new_socket);
+    }
+
+    close(server_fd);
     return 0;
 }
